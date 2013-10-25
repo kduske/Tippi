bfa4fb573cdfb0d10f6f14a423c2d7f1742ed87e
Kristian Duske <kristian.duske@gmail.com>
2013-10-25 16:22:10 +0200

    Add GPL and notice.

--- src/CollectionUtils.h
+++ src/CollectionUtils.h
@@ -1,24 +1,24 @@
 /*
- Copyright (C) 2010-2013 Kristian Duske
+ Copyright (C) 2013 Kristian Duske
  
- This file is part of TrenchBroom.
+ This file is part of Tippi.
  
- TrenchBroom is free software: you can redistribute it and/or modify
+ Tippi is free software: you can redistribute it and/or modify
  it under the terms of the GNU General Public License as published by
  the Free Software Foundation, either version 3 of the License, or
  (at your option) any later version.
  
- TrenchBroom is distributed in the hope that it will be useful,
+ Tippi is distributed in the hope that it will be useful,
  but WITHOUT ANY WARRANTY; without even the implied warranty of
  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  GNU General Public License for more details.
  
  You should have received a copy of the GNU General Public License
- along with TrenchBroom. If not, see <http://www.gnu.org/licenses/>.
+ along with Tippi. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef TrenchBroom_CollectionUtils_h
-#define TrenchBroom_CollectionUtils_h
+#ifndef Tippi_CollectionUtils_h
+#define Tippi_CollectionUtils_h
 
 #include <algorithm>
 #include <iterator>
--- src/Net/LolaNetParser.h
+++ src/Net/LolaNetParser.h
@@ -1,10 +1,21 @@
-//
-//  LolaNetParser.h
-//  Tippi
-//
-//  Created by Kristian Duske on 29.08.12.
-//  Copyright (c) 2012 TU Berlin. All rights reserved.
-//
+/*
+ Copyright (C) 2013 Kristian Duske
+ 
+ This file is part of Tippi.
+ 
+ Tippi is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+ 
+ Tippi is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+ 
+ You should have received a copy of the GNU General Public License
+ along with Tippi. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #ifndef __Tippi__LolaNetParser__
 #define __Tippi__LolaNetParser__
